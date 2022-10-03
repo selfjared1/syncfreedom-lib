f4667d71ccab7d1d6b9a35a7fd89925e5c1cf554
Jared Self <self.jared@gmail.com>
2022-10-03 14:41:34 -0600

    Adding skeleton to settings

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'syncfreedom-lib',
+    packages = ['syncfreedom-lib'],
+    version = '0.1',  # Ideally should be same as your GitHub release tag varsion
+    description = 'Initial Release',
+    author = 'Jared Self',
+    author_email = 'jared@quickbooksgg.com',
+    url = 'https://github.com/selfjared1/syncfreedom-lib/releases/tag/0.1',
+    download_url = 'https://github.com/selfjared1/syncfreedom-lib/archive/refs/tags/0.1.zip',
+    keywords = ['quickbooks online', 'quickbooks'],
+    classifiers = [],
+)
